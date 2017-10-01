c1ef2a339b6e26cefd25e1002a0b5a5351abbf1f
Mark Joshua Bihag <markjoshuabihag@gmail.com>
2017-09-30 22:34:37 -0700

    added skills section

--- scripts.js
+++ scripts.js
@@ -13,3 +13,12 @@ $(document).ready(function(){
    });
     }
 });
+
+
+$(document).ready(function(){
+  $('.skillbar').each(function(){
+    $(this).find('.skillbar-bar').animate({
+      width:$(this).attr('data-percent')
+    },6000);
+  });
+});
